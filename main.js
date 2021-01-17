e332dc038be6913cd2ebd4e9992391bece0f7118
bipingosain <bipingosain3492@gmail.com>
2021-01-17 15:11:13 +0530

    basic structure site completed, added project and contact section

---
+++ main.js
@@ -0,0 +1,36 @@
+window.onload = ()=>{
+    listenToCSSEvents();
+}
+
+
+function listenToCSSEvents(){
+
+    //TECH EVENTS
+    document.querySelectorAll('.tech-img').forEach(
+        x=> x.addEventListener('mouseover', (eve)=>{
+            console.log('here');
+            eve.target.classList.add('jello-horizontal');
+        })
+    )
+
+    document.querySelectorAll('.tech-img').forEach(
+        x=> x.addEventListener('mouseleave', (eve)=>{
+            eve.target.classList.remove('jello-horizontal');
+        })
+    )
+    
+    
+    //PORTFOLIO EVENTS
+
+    document.querySelectorAll('.project-img').forEach(
+        x=> x.addEventListener('mouseover', (eve)=>{
+            eve.target.classList.add('jello-horizontal');
+        })
+    )
+
+    document.querySelectorAll('.project-img').forEach(
+        x=> x.addEventListener('mouseleave', (eve)=>{
+            eve.target.classList.remove('jello-horizontal');
+        })
+    )
+}

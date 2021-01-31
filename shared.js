00c16e3941cf25dd983822b2d620353e70043750
Bipin <bipin@Bipins-MacBook-Air.local>
2021-01-31 18:30:03 +0530

    added mobile navigation transition

--- shared.js
+++ shared.js
@@ -1,19 +1,23 @@
 let menu = document.querySelector('.menu');
 let mobileNav = document.querySelector('.nav-burger');
-let  backButton = document.querySelector('#back-btn');
+let backdrop = document.querySelector('.backdrop');
 
 let anchors = document.querySelectorAll('.nav-burger__list__item__a');
 for(let a of anchors){
     a.addEventListener('click', ()=>{
         mobileNav.classList.remove("show");
+        backdrop.classList.remove('show');
     });
 }
 
 
 menu.addEventListener('click', ()=>{
     mobileNav.classList.add("show");
+    mobileNav.classList.add("slide-right");
+    backdrop.classList.add('show')
 });
 
-backButton.addEventListener('click', ()=>{
+backdrop.addEventListener('click' ,()=>{
     mobileNav.classList.remove("show");
-});
+    backdrop.classList.remove('show')
+})
